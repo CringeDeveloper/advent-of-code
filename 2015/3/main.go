027963ddc0eab3b3dc3b0106ec2a220efdf9229f
Vladislav <warzon131@gmail.com>
2025-03-07 03:06:06 +0200

    temp 2

--- 2015/3/main.go
+++ 2015/3/main.go
@@ -8,41 +8,75 @@ type compose struct {
 }
 
 func main() {
-	x := 0
-	y := 0
+	santaX := 0
+	santaY := 0
+	robotX := 0
+	robotY := 0
 	delivered := 1
 	m := make(map[compose]int)
-	m[compose{x, y}] = 1
+	m[compose{santaX, santaY}] = 1
+	turn := 0
 
 	input := []rune("^v^v^v^v^v")
 
 	for _, v := range input {
-		switch v {
-		case '^':
-			y++
-			if _, ok := m[compose{x, y}]; !ok {
-				m[compose{x, y}] = 1
-				delivered++
+		if turn%2 == 0 {
+			switch v {
+			case '^':
+				santaY++
+				if _, ok := m[compose{santaX, santaY}]; !ok {
+					m[compose{santaX, santaY}] = 1
+					delivered++
+				}
+			case 'v':
+				santaY--
+				if _, ok := m[compose{santaX, santaY}]; !ok {
+					m[compose{santaX, santaY}] = 1
+					delivered++
+				}
+			case '<':
+				santaX--
+				if _, ok := m[compose{santaX, santaY}]; !ok {
+					m[compose{santaX, santaY}] = 1
+					delivered++
+				}
+			case '>':
+				santaX++
+				if _, ok := m[compose{santaX, santaY}]; !ok {
+					m[compose{santaX, santaY}] = 1
+					delivered++
+				}
 			}
-		case 'v':
-			y--
-			if _, ok := m[compose{x, y}]; !ok {
-				m[compose{x, y}] = 1
-				delivered++
-			}
-		case '<':
-			x--
-			if _, ok := m[compose{x, y}]; !ok {
-				m[compose{x, y}] = 1
-				delivered++
-			}
-		case '>':
-			x++
-			if _, ok := m[compose{x, y}]; !ok {
-				m[compose{x, y}] = 1
-				delivered++
+		} else {
+			switch v {
+			case '^':
+				robotY++
+				if _, ok := m[compose{robotX, robotY}]; !ok {
+					m[compose{robotX, robotY}] = 1
+					delivered++
+				}
+			case 'v':
+				robotY--
+				if _, ok := m[compose{robotX, robotY}]; !ok {
+					m[compose{robotX, robotY}] = 1
+					delivered++
+				}
+			case '<':
+				robotX--
+				if _, ok := m[compose{robotX, robotY}]; !ok {
+					m[compose{robotX, robotY}] = 1
+					delivered++
+				}
+			case '>':
+				robotX++
+				if _, ok := m[compose{robotX, robotY}]; !ok {
+					m[compose{robotX, robotY}] = 1
+					delivered++
+				}
 			}
 		}
+
+		turn++
 	}
 
 	fmt.Println(delivered)
