8e86ddaf014b0fd875c376071f6e8ec33ebfd86a
Vladislav <warzon131@gmail.com>
2025-03-08 00:48:45 +0200

    update

--- 2015/4/main.go
+++ 2015/4/main.go
@@ -7,27 +7,18 @@ import (
 )
 
 func main() {
-	input := []byte("yzbqklnj")
+	input := "yzbqklnj"
 	code := 0
-	f := 0
 
 	for {
-		codeS := strconv.Itoa(code)
-		temp := append(input, []byte(codeS)...)
-		result := fmt.Sprintf("%x", md5.Sum(temp))
-		for _, v := range []rune(result[:6]) {
-			if v == '0' {
-				f++
-			}
-
-			if f == 6 {
-				fmt.Println(codeS)
-				fmt.Println(result)
-				return
-			}
+		codeStr := strconv.Itoa(code)
+		result := fmt.Sprintf("%x", md5.Sum([]byte(input+codeStr)))
+		if result[:6] == "000000" {
+			fmt.Println(codeStr)
+			fmt.Println(result)
+			return
 		}
 
-		f = 0
 		code++
 	}
 }
